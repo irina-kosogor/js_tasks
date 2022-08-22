db3b48c88d1ecfcbcbca0a25ac3cf0f9944600f0
Iryna Kosogor <i.v.kosogor@gmail.com>
2022-08-22 17:22:44 +0300

    second practical task is done

--- JS/script.js
+++ JS/script.js
@@ -9,12 +9,28 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt("What is the last movie you have watched ?", ""),
-      b = prompt("From 1 to 5 how you would rate it?", ""),
-      c = prompt("What is the last movie you have watched ?", ""),
-      d = prompt("From 1 to 5 how you would rate it?", "");
+for (let i = 0; i < 2; i++) {
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    const a = prompt("What is the last movie you have watched ?", "");
+    const b = prompt("From 1 to 5 how you would rate it?", "");
+
+    if (a != null && a != "" && a.length < 50 && b != null && b != "" && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("You have seen just a few movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("You are a classy films lover");
+} else if (personalMovieDB.count >= 30) {
+    alert("You are a real films lover");
+} else {
+    alert("Error");
+}
 
 console.log(personalMovieDB);
